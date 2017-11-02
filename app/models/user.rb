f98da67ad103f2e6dced0280960e4874ec1e64d8
JunePaloma <kborkoski@gmail.com>
2017-11-02 16:07:13 -0600

    Github OmniAuth working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
-class User < ApplicationController
+class User < ApplicationRecord
 
   def self.find_or_create_from_auth(auth)
-    binding.pry
-    user = User.find_or_create_by(github_uid: auth['uid'])
-      user.name         = auth['info']['name']
-      user.github_uid   = auth['uid']
-      user.github_token = auth['credentials']['token']
+      user =  User.find_or_create_by(github_uid: auth.uid)
+      user.name         = auth.info.name
+      user.github_uid   = auth.uid
+      user.github_token = auth.credentials.token
+      user.save
+      user
   end
+
 end
