ef3c131ef53943594625977a5eb57bc931003092
JunePaloma <kborkoski@gmail.com>
2017-11-02 18:00:48 -0600

    log in test is working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
 
   def self.find_or_create_from_auth(auth)
-      user =  User.find_or_create_by(github_uid: auth.uid)
-      user.name         = auth.info.name
-      user.github_uid   = auth.uid
-      user.github_token = auth.credentials.token
+      user =  User.find_or_create_by(github_uid: auth['info']['uid'])
+      user.name         = auth['info']['name']
+      user.github_uid   = auth['uid']
+      user.github_token = auth['credentials']['token']
       user.save
       user
   end
