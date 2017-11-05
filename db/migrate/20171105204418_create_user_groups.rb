50e55cbd707c1040d4505bb533b26f08b601c438
JunePaloma <kborkoski@gmail.com>
2017-11-05 13:51:15 -0700

    Add user groups

--- db/migrate/20171105204418_create_user_groups.rb
+++ db/migrate/20171105204418_create_user_groups.rb
@@ -1,8 +1,8 @@
 class CreateUserGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :user_groups do |t|
-      t.user :references
-      t.group :references
+      t.references :user, foreign_key: true
+      t.references :group, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105204151) do
+ActiveRecord::Schema.define(version: 20171105204418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20171105204151) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_groups", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_user_groups_on_group_id"
+    t.index ["user_id"], name: "index_user_groups_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "github_token"
     t.string "github_uid"
@@ -36,4 +45,6 @@ ActiveRecord::Schema.define(version: 20171105204151) do
   end
 
   add_foreign_key "groups", "projects"
+  add_foreign_key "user_groups", "groups"
+  add_foreign_key "user_groups", "users"
 end
