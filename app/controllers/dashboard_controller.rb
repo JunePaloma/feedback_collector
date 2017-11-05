3823cb26499685a09b583bef37f5259a7323df9f
JunePaloma <kborkoski@gmail.com>
2017-11-05 15:45:05 -0700

    re-fixed the relationships on the user_group model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class DashboardController < ApplicationController
   def index
-    binding.pry
     @groups = current_user.groups
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,5 @@
 class DashboardController < ApplicationController
-
   def show
     @users = current_user.team_members(params[:id])
   end
-
 end
